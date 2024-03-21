8c318dbb7152759349ae353ae54ba7a7703163a5
sdedu <sdedu@DESKTOP-7510KTC>
2024-03-21 09:10:03 +0900

    03.21자바30일차

---
+++ Web01_HTML/com/beans/dto/Product.java
@@ -0,0 +1,17 @@
+
+public class Product {
+
+	private int quantity;	//장바구니에 담은 개수
+
+	//장바구니
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+	
+}
---
+++ Web03_JavaScript/src/main/webapp/js/test.js
@@ -0,0 +1,7 @@
+/**
+ * 
+ */
+document.write('<p>반가워요!</p>');
+document.write('<p>당신의 첫 번째\"자바스크립트\"입니다.</p>');
+document.write('<p>반가워요!</p>');
+ 
